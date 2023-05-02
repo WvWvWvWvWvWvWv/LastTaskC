c3487aee32628a93ab4082503991517c17e730cd
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-05-02 11:46:25 +0300

    Add Task

---
+++ Task/Program.cs
@@ -0,0 +1,29 @@
+﻿void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+
+
+    }
+}
+
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] array1 = new string[4] { "Hello", "2", "world", ":-" };
+string[] array2 = new string[array1.Length];
+SecondArray(array1, array2);
+PrintArray(array2);
